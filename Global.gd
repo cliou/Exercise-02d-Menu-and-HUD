4e3e6d2aa3254e6fbed1d27a6400bd89296637ba
CrystalLiou <cliou@iu.edu>
2022-09-17 11:53:18 -0700

    Exercise completed

--- Global.gd
+++ Global.gd
@@ -1,8 +1,64 @@
 extends Node
 
+var VP = Vector2.ZERO
+
+var score = 0
+var time = 100
+var lives = 5
+
 func _ready():
+	pause_mode = Node.PAUSE_MODE_PROCESS
 	randomize()
+	VP = get_viewport().size
+	var _signal = get_tree().get_root().connect("size_changed", self, "_resize")
+
+func _resize():
+	VP = get_viewport().size
+
+func reset():
+	get_tree().paused = false
+	score = 0
+	time = 30
+	lives = 5
+
+func update_score(s):
+	score += s
+	var hud = get_node_or_null("/root/Game/UI/HUD")
+	if hud != null:
+		hud.update_score()
+
+func update_time(t):
+	time += t
+	if time <= 0:
+		var _scene = get_tree().change_scene("res://UI/End_Game.tscn")
+	var hud = get_node_or_null("/root/Game/UI/HUD")
+	if hud != null:
+		hud.update_score()
+		
+func update_lives(l):
+	lives += l
+	if lives <= 0:
+		var _scene = get_tree().change_scene("res://UI/End_Game.tscn")
+	var hud = get_node_or_null("/root/Game/UI/HUD")
+	if hud != null:
+		hud.update_score()
+
+func _physics_process(_delta):
+	var Asteroid_Container = get_node_or_null("/root/Game/Asteroid_Container")
+	var Enemy_Container = get_node_or_null("/root/Game/Enemy_Container")
+	if Asteroid_Container != null and Enemy_Container != null:
+		if Asteroid_Container.get_child_count() == 0 and Enemy_Container.get_child_count() == 0:
+			var _scene = get_tree().change_scene("res://UI/End_Game.tscn")
 
 func _unhandled_input(event):
 	if event.is_action_pressed("menu"):
-		get_tree().quit()
+		var Menu = get_node_or_null("/root/Game/UI/Menu")
+		if Menu == null:
+			get_tree().quit()
+		else:
+			if Menu.visible:
+				get_tree().paused = false
+				Menu.hide()
+			else:
+				get_tree().paused = true
+				Menu.show()
